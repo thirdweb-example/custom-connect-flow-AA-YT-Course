b4b20fa7d539b8b5f715ae1acb4b1dfd8e5745ae
watase-eth <seankwatase@gmail.com>
2023-11-11 08:56:58 -1000

    Initial Commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,27 @@
 import type { AppProps } from "next/app";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { ThirdwebProvider, embeddedWallet, metamaskWallet, smartWallet } from "@thirdweb-dev/react";
 import "../styles/globals.css";
+import { ACCOUNT_FACTORY_ADDRESS } from "../constants/addresses";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+const activeChain = "mumbai";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const smartWalletConfig = {
+    factoryAddress: ACCOUNT_FACTORY_ADDRESS,
+    gasless: true,
+  }
+
   return (
     <ThirdwebProvider
       clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
       activeChain={activeChain}
+      supportedWallets={[
+        smartWallet(embeddedWallet(), smartWalletConfig),
+        smartWallet(metamaskWallet(), smartWalletConfig),
+      ]}
     >
       <Component {...pageProps} />
     </ThirdwebProvider>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,106 +1,125 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { ConnectWallet, Web3Button, embeddedWallet, metamaskWallet, smartWallet, useAddress, useConnect, useContract, useOwnedNFTs } from "@thirdweb-dev/react";
 import styles from "../styles/Home.module.css";
-import Image from "next/image";
 import { NextPage } from "next";
+import { ACCOUNT_FACTORY_ADDRESS, NFT_CONTRACT_ADDRESS } from "../constants/addresses";
+import { useState } from "react";
+
+const embeddedWalletConfig = embeddedWallet({
+  styles: {
+    borderRadius: "10px",
+    colorBackground: "#232323",
+    colorPrimary: "lightseagreen",
+    colorText: "#FFFFFF",
+  }
+});
+
+const metamaskWalletConfig = metamaskWallet();
+
+const smartWalletConfig = smartWallet(embeddedWalletConfig, {
+  factoryAddress: ACCOUNT_FACTORY_ADDRESS,
+  gasless: true,
+});
 
 const Home: NextPage = () => {
+  const address = useAddress();
+  const connect = useConnect();
+
+  const [emailInput, setEmailInput] = useState("");
+  const [personalWalletAddress, setPersonalWalletAddress] = useState<string | undefined>(undefined);
+  const [smartWalletAddress, setSmartWalletAddress] = useState<string | undefined>(undefined);
+
+  const handleLogin = async () => {
+    try {
+      const personalWallet = await connect(metamaskWalletConfig);
+      const personalWalletAddress = await personalWallet.getAddress();
+      setPersonalWalletAddress(personalWalletAddress);
+
+      const smartWallet = await connect(smartWalletConfig, {
+        personalWallet: personalWallet,
+        chainId: 80001,
+      });
+      const smartWalletAddress = await smartWallet.getAddress();
+      setSmartWalletAddress(smartWalletAddress);
+
+      setEmailInput("");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const {
+    contract
+  } = useContract(NFT_CONTRACT_ADDRESS);
+
+  const {
+    data: personalOwnedNFTs,
+    isLoading: isPersonalOwnedNFTsLoading,
+  } = useOwnedNFTs(contract, personalWalletAddress);
+
+  const {
+    data: smartOwnedNFTs,
+    isLoading: isSmartOwnedNFTsLoading,
+  } = useOwnedNFTs(contract, smartWalletAddress);
+
   return (
     <main className={styles.main}>
       <div className={styles.container}>
-        <div className={styles.header}>
-          <h1 className={styles.title}>
-            Welcome to{" "}
-            <span className={styles.gradientText0}>
-              <a
-                href="https://thirdweb.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                thirdweb.
-              </a>
-            </span>
-          </h1>
-
-          <p className={styles.description}>
-            Get started by configuring your desired network in{" "}
-            <code className={styles.code}>src/index.js</code>, then modify the{" "}
-            <code className={styles.code}>src/App.js</code> file!
-          </p>
-
-          <div className={styles.connect}>
-            <ConnectWallet
-              dropdownPosition={{
-                side: "bottom",
-                align: "center",
-              }}
-            />
+        {address ? (
+          <div className={styles.centeredContainer}>
+            <div className={styles.centeredCard}>
+              <ConnectWallet />
+              <h1>Logged in!</h1>
+              <p>Personal Wallet: {personalWalletAddress}</p>
+              <Web3Button
+                contractAddress={NFT_CONTRACT_ADDRESS}
+                action={(contract) => contract.erc1155.claimTo(personalWalletAddress!, 0, 1)}
+              >Claim NFT</Web3Button>
+              <div style={{ width: "100%", margin: "0 auto", textAlign: "center" }}>
+                {!isPersonalOwnedNFTsLoading && (
+                  personalOwnedNFTs && personalOwnedNFTs.length > 0 ? (
+                    personalOwnedNFTs.map((nft) => (
+                      <p key={nft.metadata.id}>QTY: {nft.quantityOwned}</p>
+                    ))
+                  ) : (
+                    <p>No NFTs owned.</p>
+                  )
+                )}
+              </div>
+              <p>Smart Wallet: {smartWalletAddress}</p>
+              <Web3Button
+                contractAddress={NFT_CONTRACT_ADDRESS}
+                action={(contract) => contract.erc1155.claim(0, 1)}
+              >Claim NFT</Web3Button>
+              <div style={{ width: "100%", margin: "0 auto", textAlign: "center" }}>
+                {!isSmartOwnedNFTsLoading && (
+                  smartOwnedNFTs && smartOwnedNFTs.length > 0 ? (
+                    smartOwnedNFTs.map((nft) => (
+                      <p key={nft.metadata.id}>QTY: {nft.quantityOwned}</p>
+                    ))
+                  ) : (
+                    <p>No NFTs owned.</p>
+                  )
+                )}
+              </div>
+            </div>
           </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://portal.thirdweb.com/"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/portal-preview.png"
-              alt="Placeholder preview of starter"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText1}>Portal ➜</h2>
-              <p>
-                Guides, references, and resources that will help you build with
-                thirdweb.
-              </p>
+        ) : (
+          <div className={styles.centeredContainer}>
+            <div className={styles.centeredCard}>
+              <h1>Login</h1>
+              <p>Enter your email to login.</p>
+              <input 
+                type="email"  
+                placeholder="Enter your email"
+                value={emailInput}
+                onChange={(e) => setEmailInput(e.target.value)}
+              />
+              <button
+                onClick={handleLogin}
+              >Login</button>
             </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/dashboard"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/dashboard-preview.png"
-              alt="Placeholder preview of starter"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText2}>Dashboard ➜</h2>
-              <p>
-                Deploy, configure, and manage your smart contracts from the
-                dashboard.
-              </p>
-            </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/templates"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/templates-preview.png"
-              alt="Placeholder preview of templates"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText3}>Templates ➜</h2>
-              <p>
-                Discover and clone template projects showcasing thirdweb
-                features.
-              </p>
-            </div>
-          </a>
-        </div>
+          </div>
+        )}
       </div>
     </main>
   );
